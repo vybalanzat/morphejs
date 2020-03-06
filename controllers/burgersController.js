8ecaa29e50f1b9c0a4078d9505888a1c97cae4c3
Vy Balanzat <vybalanzat@gmail.com>
2020-03-05 22:35:56 -0500

    test

---
+++ controllers/burgersController.js
@@ -0,0 +1,55 @@
+var express = require("express");
+
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+// get route -> index
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+  // express callback response by calling burger.selectAllBurger
+  burger.all(function(burgerData) {
+    // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+    res.json({ burgers: burgerData });
+  });
+});
+
+// post route -> back to index
+router.post("/burgers", function(req, res) {
+  // takes the request object using it as input for burger.addBurger
+  burger.create(req.body.burger_name, function(result) {
+    // wrapper for orm.js that using MySQL insert callback will return a log to console,
+    // render back to index with handle
+    console.log(result);
+    res.json({ id: result.id });
+  });
+});
+
+// put route -> back to index
+router.put("/burgers/:id", function(req, res) {
+  burger.update(req.params.id, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.json({ id: req.params.id });
+    }
+  });
+});
+
+router.delete("/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,27 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  create: function(name, cb) {
+    orm.create("burgers", [
+      "burger_name", "devoured"
+    ], [
+      name, false
+    ], cb);
+  },
+  update: function(id, cb) {
+    var condition = "id=" + id;
+    orm.update("burgers", {
+      devoured: true
+    }, condition, cb);
+  },
+  delete: function(condition,cb) {
+    orm.delete("burgers", condition, cb);
+  }
+};
+
+module.exports = burger;
